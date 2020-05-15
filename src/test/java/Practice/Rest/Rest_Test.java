cfeb9c9d469a8dea9d19f6da5c3bb0f2c8d1e1e5
Ashish Mathur <ashishmathur@DLLA38P3561.qait.com>
2020-05-15 16:39:08 +0530

    Updated JUNIT dependency

--- src/test/java/Practice/Rest/Rest_Test.java
+++ src/test/java/Practice/Rest/Rest_Test.java
@@ -1,21 +1,19 @@
 package Practice.Rest;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import junit.framework.Assert;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class Rest_Test {
 	
 	App app = new App();
-
-	@SuppressWarnings("deprecation")
 	@Test
 	public void verifyUrlContainsData() {
 		
 		given().when().get("https://reqres.in/api/users/2").then().statusCode(200).assertThat().body(containsString("janet.weaver@reqres.in"));
-		
+		Assert.assertEquals(true, true);
 		Assert.assertTrue(app.getUserData("2").contains("janet.weaver@reqres.in"));
 		Assert.assertTrue(app.getUserData("3").contains("emma.wong@reqres.in"));
 		Assert.assertTrue(app.getUserData("4").contains("eve.holt@reqres.in"));
